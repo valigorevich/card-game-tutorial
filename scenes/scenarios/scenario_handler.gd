559ec2a151ee42706ae48aea3bdfc18e4c3439d2
v.prokofev <prokofev@corp.mail.ru>
2023-12-30 12:20:58 +0300

    Safety checks in scenario handler

--- scenes/scenarios/scenario_handler.gd
+++ scenes/scenarios/scenario_handler.gd
@@ -11,6 +11,10 @@ func _ready() -> void:
 
 #we pass a scenario to pick proper stats to spawn
 func spawn_wave(scenario: BattleScenario, level: int) -> void:
+	if not scenario:
+		return
+	
+	#Check if wave level exeedes number of waves
 	if level > scenario.enemy_spawner.size():
 		#Emit signal that we finished the run to handle win condition
 		return
@@ -19,6 +23,10 @@ func spawn_wave(scenario: BattleScenario, level: int) -> void:
 	var new_enemy_spawner := new_enemy_wave.select_wave() as EnemySpawner
 	var enemy_index := 0
 	for enemy in new_enemy_spawner.enemies:
+		#Check if we have more enemies than markers on level
+		if enemy_index > positions.size():
+			return
+		
 		var position_marker := positions[enemy_index] as Marker2D
 		enemy_handler.spawn_enemy(enemy, position_marker.global_position)
 		enemy_index += 1
