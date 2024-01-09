0dd92e6289bdb7d26c9c2ecde481ffec14d8a0e2
Your Name <you@example.com>
2024-01-09 21:20:02 +0300

    fix

--- scenes/battle/deck_info_panel.gd
+++ scenes/battle/deck_info_panel.gd
@@ -3,7 +3,6 @@ extends Panel
 @onready var player = %Player
 @onready var card_list_panel = %CardListPanel
 @onready var cards_quantity_label = %NumCards
-# @onready var card_ui = preload("res://scenes/card_ui/card_ui.tscn")
 @onready var item_ui = preload("res://scenes/item_ui/item_ui.tscn")
 
 var list_counter = 0
@@ -17,15 +16,12 @@ func _on_deck_info_open_button_pressed():
 	var cards_count = len(card_list.cards)
 	cards_quantity_label.text = "There are %s cards in deck" % cards_count
 	print("quantity", cards_count)
-	# var stats = player.stats
 	for card in card_list.cards:
 		if list_counter < cards_count:
 			var item_ui_instance := item_ui.instantiate()
 			card_list_panel.add_child(item_ui_instance)
-			# item_ui_instance.character_stats = stats
 			item_ui_instance.item = card
 			item_ui_instance.parent = self
-			# item_ui_instance.preview_mode = true
 			item_ui_instance.disabled = true
 			item_ui_instance.global_position += card_list_panel.global_position + Vector2(10, 10)
 			item_ui_instance.selectable = false
--- scenes/ui/choose_item_panel.gd
+++ scenes/ui/choose_item_panel.gd
@@ -2,7 +2,6 @@ class_name ChooseItemPanel
 extends GridContainer
 
 var list_counter = 0
-# @onready var card_ui = preload("res://scenes/card_ui/card_ui.tscn")
 
 @onready var item_ui = preload("res://scenes/item_ui/item_ui.tscn")
 
@@ -14,10 +13,8 @@ func render(
 		for card in card_list.cards:
 			var item_ui_instance := item_ui.instantiate()
 			self.add_child(item_ui_instance)
-			# item_ui_instance.character_stats = stats
 			item_ui_instance.item = card
 			item_ui_instance.parent = self
-			# item_ui_instance.preview_mode = true
 			item_ui_instance.disabled = true
 			item_ui_instance.selectable = true
 			list_counter += 1
